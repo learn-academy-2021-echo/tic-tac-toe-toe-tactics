bcb71d050739d989d6140f24909413a0d0fe1d9d
WWDavidson <w.w.davidson33@gmail.com>
2022-01-05 16:07:32 -0600

    got grid and minor logic working

--- src/App.js
+++ src/App.js
@@ -6,17 +6,46 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: ["", "", "", "", "", "", "", "", ""],
+      sqArry:  [],
     }
   }
+//we need to manipulate the code so that both x's and o's show up on our gameboard
+
+  handleGamePlay = (index) => {
+    const { squares, sqArry } = this.state
+    if(index === sqArry) {
+      squares[index] = "❌"
+      this.setState({squares: squares})
+    } else {
+    squares[index] = "〇"
+    this.setState({squares: squares})
+    }  
+  }
+
+
+
+
 
   render(){
-    return(
-      <>
-        <h1>Tic Tac Toe</h1>
-        <Square />
-      </>
-    )
-  }
-}
+    
+    
+        return(
+          <>
+            <h1>Treasure Hunt Game</h1>
+            <div className="gameboard">
+            {this.state.squares.map((value, index) => {
+              return (
+                <Square 
+                value={value} 
+                index={index}
+                handleGamePlay={this.handleGamePlay}
+                />
+              )
+            })}
+            </div>
+          </>
+        )
+      }
+    }
 export default App
